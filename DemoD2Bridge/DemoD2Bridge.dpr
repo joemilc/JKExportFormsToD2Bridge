f3dc748ecb146f16982d84c1f2acd456a69b26aa
Joemil <joemilc@gmail.com>
2024-11-05 19:54:22 -0400

    ajustes no demo

--- DemoD2Bridge/DemoD2Bridge.dpr
+++ DemoD2Bridge/DemoD2Bridge.dpr
@@ -14,7 +14,11 @@ uses
   DemoD2Bridge_Session in 'DemoD2Bridge_Session.pas' {DemoD2BridgeSession},
   D2BridgeFormTemplate in 'D2BridgeFormTemplate.pas',
   Unit_D2Bridge_Server_Console in 'Unit_D2Bridge_Server_Console.pas',
-  Unit1 in '..\ProjetoDemo\Unit1.pas' {Form1};
+  Unit1 in '..\ProjetoDemo\Unit1.pas' {Form1},
+  uFormComplexo in '..\ProjetoDemo\uFormComplexo.pas' {FormComplexo},
+  uFormMedio in '..\ProjetoDemo\uFormMedio.pas' {FormMedio},
+  uFormSimples in '..\ProjetoDemo\uFormSimples.pas' {FormSimples},
+  Unit2 in '..\ProjetoDemo\Unit2.pas' {Form2};
 
 {$R *.res}
 
